e59444c5363ac6ecf2fe40d108e70fe979dce9b5
Emilio <emiliodaniel02@hotmail.com>
2024-11-30 23:23:41 -0500

    Added aws-exports.js

---
+++ aws-exports.js
@@ -0,0 +1,10 @@
+const awsmobile = {
+    Storage: {
+        AWSS3: {
+            bucket: "receiptstoragede97a-dev", // Replace with your S3 bucket name
+            region: "us-east-1",     // Replace with your AWS region (e.g., us-east-1)
+        }
+    }
+};
+
+export default awsmobile;
